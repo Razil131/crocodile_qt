ccad5e516a5370243376ae067192791ba7b56bb0
Ilya <razilvel@gmail.com>
2026-05-07 18:10:47 +0700

    ui fix test

--- gamewindow.cpp
+++ gamewindow.cpp
@@ -1,13 +1,11 @@
 #include "gamewindow.h"
 #include "ui_gamewindow.h"
 
-
 GameWindow::GameWindow(QWidget *parent)
     : QMainWindow(parent)
     , ui(new Ui::GameWindow)
 {
     ui->setupUi(this);
-    ui->Canvas->hide();
 }
 
 GameWindow::~GameWindow()
